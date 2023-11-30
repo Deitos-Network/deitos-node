2cecae2a8d19954963e6be55336b818a7f3460a8
Hector Bulgarini <hbulgarini@gmail.com>
2023-11-30 16:29:41 +0100

    added proposal for extrinsics api

--- pallets/deitos/src/lib.rs
+++ pallets/deitos/src/lib.rs
@@ -32,14 +32,45 @@ pub mod pallet {
     pub trait Config: frame_system::Config {
         /// The overarching runtime event type.
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
         /// A type representing the weights required by the dispatchables of this pallet.
         type WeightInfo: WeightInfo;
+
+        type AgreementId: Member
+            + Parameter
+            + Copy
+            + Clone
+            + MaybeSerializeDeserialize
+            + MaxEncodedLen
+            + Saturating
+            + One
+            + Zero;
+
+        /// Maximum Plan Duration
+        #[pallet::constant]
+        type MaxPaymentPlanDuration: Get<u32>;
+
+        #[pallet::constant]
+        type PalletId: Get<PalletId>;
     }
 
     #[pallet::storage]
     #[pallet::getter(fn get_ip)]
     pub type InfrastructureProvider<T: Config> =
-        StorageMap<_, BlakeTwo256, T::AccountId, InfraProviderDetails<T>>;
+        StorageMap<_, Blake2_128Concat, T::AccountId, InfraProviderDetails<T>>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn get_agreements)]
+    pub type Agreements<T: Config> = StorageNMap<
+        _,
+        (
+            NMapKey<Blake2_128Concat, T::AccountId>,   // consumer
+            NMapKey<Blake2_128Concat, T::AccountId>,   // infrastructure provider
+            NMapKey<Blake2_128Concat, T::AgreementId>, // agreement id
+        ),
+        Agreement<T>,
+        ResultQuery<Error<T>::NonExistentStorageValue>,
+    >;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -95,6 +126,148 @@ pub mod pallet {
         #[pallet::call_index(3)]
         #[pallet::weight(T::WeightInfo::shutdown_ip())]
         pub fn shutdown_ip(origin: OriginFor<T>, price_unit: BalanceOf<T>) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(4)]
+        #[pallet::weight(T::WeightInfo::shutdown_ip())]
+        pub fn submit_agreement_request(
+            origin: OriginFor<T>,
+            provider: AccountIdLookupOf<T>,
+            agreement: Agreement<T>,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(5)]
+        #[pallet::weight(T::WeightInfo::ip_agreement_reponse())]
+        pub fn ip_agreement_reponse(
+            origin: OriginFor<T>,
+            consumer: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+            agreement: Agreement<T>,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            // Accepts the agreement and payment plan
+            // Accepts the agreement and propose a payment plan with modifications
+            // Rejects the agreement and payment plan
+
+            Ok(())
+        }
+
+        #[pallet::call_index(5)]
+        #[pallet::weight(T::WeightInfo::consumer_agreement_reponse())]
+        pub fn consumer_agreement_reponse(
+            origin: OriginFor<T>,
+            provider: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+            agreement: Agreement<T>,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            // Accepts the agreement if IP accepts everything without modifications
+            // Accepts the payment plan with modifications
+            // Rejects the plan with modifications
+
+            Ok(())
+        }
+
+        #[pallet::call_index(5)]
+        #[pallet::weight(T::WeightInfo::consumer_cancels_agreement())]
+        pub fn consumer_cancels_agreement(
+            origin: OriginFor<T>,
+            provider: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+            agreement: Agreement<T>,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(6)]
+        #[pallet::weight(T::WeightInfo::ip_cancels_agreement())]
+        pub fn ip_cancels_agreement(
+            origin: OriginFor<T>,
+            consumer: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+            agreement: Agreement<T>,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(6)]
+        #[pallet::weight(T::WeightInfo::make_installment_payment())]
+        pub fn make_installment_payment(
+            origin: OriginFor<T>,
+            provider: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(7)]
+        #[pallet::weight(T::WeightInfo::withdraw_provider_funds())]
+        pub fn withdraw_provider_funds(
+            origin: OriginFor<T>,
+            consumer: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(8)]
+        #[pallet::weight(T::WeightInfo::ip_withdraw())]
+        pub fn withdraw_provider_funds(
+            origin: OriginFor<T>,
+            consumer: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(8)]
+        #[pallet::weight(T::WeightInfo::submit_provider_feedback())]
+        pub fn submit_provider_feedback(
+            origin: OriginFor<T>,
+            consumer: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+        ) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            Ok(())
+        }
+
+        #[pallet::call_index(9)]
+        #[pallet::weight(T::WeightInfo::submit_consumer_feedback())]
+        pub fn submit_consumer_feedback(
+            origin: OriginFor<T>,
+            provider: AccountIdLookupOf<T>,
+            agreement_id: T::AgreementId,
+        ) -> DispatchResult {
             // Check that the extrinsic was signed and get the signer.
             let who = ensure_signed(origin)?;
 
--- pallets/deitos/src/types.rs
+++ pallets/deitos/src/types.rs
@@ -1,10 +1,18 @@
 // Types required for pallet-deitos
+use frame_system::pallet_prelude::BlockNumberFor;
 
 pub type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+pub type Storage = u64;
+pub type AgreementTimeAllocation = u32;
+pub type Installment = BalanceOf<T>;
+pub type PaymentsDetails = (Installment, PaymentPlanPeriods);
+pub type PaymentPlan = BoundedVec<PaymentsDetails<T>, MaxPaymentPlanDuration<T>>;
+
 
 
 // TO-DO: Review the necessary status.
+#[derive(Clone, Encode, Decode, Eq, PartialEq, MaxEncodedLen, TypeInfo, Debug)]
 pub type IPStatus = enum {
     VALIDATING,
     ACTIVE,
@@ -12,6 +20,33 @@ pub type IPStatus = enum {
     SUSPENDED,
     TERMINATED,
 }
+
+#[derive(Clone, Encode, Decode, Eq, PartialEq, MaxEncodedLen, TypeInfo, Debug)]
+pub type AgreementStatus = enum {
+    CONSUMER_REQUEST,
+    IP_ACCEPTED,
+    IP_ACCEPTED_WITH_MODIFICATIONS,
+    IP_REJECTED,
+    CONSUMER_ACCEPTED_MODIFICATIONS,
+    ACTIVATED,
+    DISPUTED,
+    CONSUMER_BREACH,
+    TERMINATED,
+    WAITING_CONSUMER_FEEDBACK,
+    WAITING_PROVIDER_FEEDBACK,
+    FINISHED,
+};
+
+#[derive(Clone, Encode, Decode, Eq, PartialEq, MaxEncodedLen, TypeInfo, Debug)]
+pub type PaymentPlanPeriods = enum {
+    WEEKLY,
+    BIWEEKLY,
+    MONTHLY,
+    QUARTERLY,
+    SEMIANNUALLY,
+    ANNUALLY
+};
+
 
 #[derive(Clone, Encode, Decode, Eq, PartialEq, Debug, MaxEncodedLen, TypeInfo)]
 #[scale_info(skip_type_params(T))]
@@ -27,7 +62,17 @@ pub struct InfraProviderDetails<T: pallet::Config> {
 #[derive(Clone, Encode, Decode, Eq, PartialEq, Debug, MaxEncodedLen, TypeInfo)]
 #[scale_info(skip_type_params(T))]
 #[codec(mel_bound(T: pallet::Config))]
-pub struct Agreements<T: pallet::Config> {
-    // Consumer account
-    pub consumer: &T::AccountId,
+pub struct Agreement<T: pallet::Config> {
+    // Agreement Status
+    pub status: AgreementStatus,
+    // Total amount of storage in the agreement expressed in bytes? 
+    pub storage: Storage,
+    // Amount of time the agreement is valid for in blocks
+    pub time_allocation: AgreementTimeAllocation,
+    // Activation block
+    pub activation_block: BlockNumberFor<T>,
+    // Payment plan
+    pub payment_plan: PaymentPlan<T>,
+    // Consumer agreement lock fee.
+    pub agreement_lock_fee: BalanceOf<T>,
 }
