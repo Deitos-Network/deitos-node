f754463040050c01a04868090e790218c37f4d04
Hector Bulgarini <hbulgarini@gmail.com>
2023-12-04 11:31:41 +0100

    Making test to pass

--- pallets/deitos/src/benchmarking.rs
+++ pallets/deitos/src/benchmarking.rs
@@ -4,7 +4,7 @@ use super::*;
 use sp_std::vec;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as Deitos;
 use frame_benchmarking::v2::*;
 use frame_system::RawOrigin;
 
@@ -13,24 +13,12 @@ mod benchmarks {
     use super::*;
 
     #[benchmark]
-    fn do_something() {
+    fn register_ip() {
         let value = 100u32.into();
         let caller: T::AccountId = whitelisted_caller();
         #[extrinsic_call]
-        do_something(RawOrigin::Signed(caller), value);
-
-        assert_eq!(Something::<T>::get(), Some(value));
+        _(RawOrigin::Signed(caller), value);
     }
 
-    #[benchmark]
-    fn cause_error() {
-        Something::<T>::put(100u32);
-        let caller: T::AccountId = whitelisted_caller();
-        #[extrinsic_call]
-        cause_error(RawOrigin::Signed(caller));
-
-        assert_eq!(Something::<T>::get(), Some(101u32));
-    }
-
-    impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+    impl_benchmark_test_suite!(Deitos, crate::mock::new_test_ext(), crate::mock::Test);
 }
--- pallets/deitos/src/mock.rs
+++ pallets/deitos/src/mock.rs
@@ -1,5 +1,9 @@
-use crate as pallet_template;
-use frame_support::traits::{ConstU16, ConstU64};
+use super::*;
+use crate as pallet_deitos;
+use frame_support::{
+    parameter_types,
+    traits::{ConstU16, ConstU32, ConstU64},
+};
 use sp_core::H256;
 use sp_runtime::{
     traits::{BlakeTwo256, IdentityLookup},
@@ -13,39 +17,69 @@ frame_support::construct_runtime!(
     pub enum Test
     {
         System: frame_system,
-        TemplateModule: pallet_template,
+        Balances: pallet_balances,
+        Deitos: pallet_deitos,
     }
 );
+
+parameter_types! {
+    pub const DeitosPalletId: PalletId = PalletId(*b"DeitosId");
+}
+
+type AccountId = u64;
+type AssetId = u32;
 
 impl frame_system::Config for Test {
     type BaseCallFilter = frame_support::traits::Everything;
     type BlockWeights = ();
     type BlockLength = ();
-    type DbWeight = ();
     type RuntimeOrigin = RuntimeOrigin;
     type RuntimeCall = RuntimeCall;
     type Nonce = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
-    type AccountId = u64;
+    type AccountId = AccountId;
     type Lookup = IdentityLookup<Self::AccountId>;
     type Block = Block;
     type RuntimeEvent = RuntimeEvent;
     type BlockHashCount = ConstU64<250>;
+    type DbWeight = ();
     type Version = ();
     type PalletInfo = PalletInfo;
-    type AccountData = ();
+    type AccountData = pallet_balances::AccountData<u64>;
     type OnNewAccount = ();
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
-    type SS58Prefix = ConstU16<42>;
+    type SS58Prefix = ();
     type OnSetCode = ();
-    type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type MaxConsumers = ConstU32<3>;
+}
+
+impl pallet_balances::Config for Test {
+    type Balance = u64;
+    type DustRemoval = ();
+    type RuntimeEvent = RuntimeEvent;
+    type ExistentialDeposit = ConstU64<1>;
+    type AccountStore = System;
+    type WeightInfo = ();
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+    type RuntimeHoldReason = RuntimeHoldReason;
+    type RuntimeFreezeReason = ();
+    type FreezeIdentifier = ();
+    type MaxHolds = ConstU32<250>;
+    type MaxFreezes = ();
 }
 
 impl pallet_deitos::Config for Test {
     type RuntimeEvent = RuntimeEvent;
     type WeightInfo = ();
+    type Currency = Balances;
+    type RuntimeHoldReason = RuntimeHoldReason;
+    type AgreementId = u32;
+    type MaxPaymentPlanDuration = ConstU32<500>;
+    type PalletId = DeitosPalletId;
 }
 
 // Build genesis storage according to the mock runtime.
